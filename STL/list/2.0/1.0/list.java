41e8edf9eaf08560068eb8ea2ac3d21bb89a9176
cpplay <2641797006@qq.com>
2019-05-21 11:00:57 +0800

    -

---
+++ STL/list/2.0/1.0/list.java
@@ -0,0 +1,210 @@
+import java.util.Comparator;
+import java.util.ArrayList;
+
+
+public class list<T>{
+
+	public abstract class iterator{
+		public T data;
+
+		public abstract iterator prev();
+		public abstract iterator next();
+	}
+
+
+	private class Node extends iterator{
+		private Node prev;
+		private Node next;
+
+		public Node(){}
+
+		public Node(T t) {
+			data = t;
+		}
+
+		public Node prev() {
+			return prev;
+		}
+
+		public Node next() {
+			return next;
+		}
+	};
+
+
+	private Node head;
+	private Node tail;
+	private int size;
+
+
+	public list() {
+		init();
+	}
+
+	private void init() {
+		head = new Node();
+		tail = new Node();
+
+		head.prev = null;
+		head.next = tail;
+
+		tail.prev = head;
+		tail.next = null;
+
+		size = 0;
+	}
+
+
+	public T front() {
+		return head.next.data;
+	}
+
+
+	public T back() {
+		return tail.prev.data;
+	}
+
+
+	public iterator begin() {
+		return head.next;
+	}
+
+
+	public iterator end() {
+		return tail;
+	}
+
+
+	public iterator rbegin() {
+		return tail.prev;
+	}
+
+
+	public iterator rend() {
+		return head;
+	}
+
+
+	public boolean empty() {
+		return size == 0;
+	}
+
+
+	public int size() {
+		return size;
+	}
+
+
+	public list<T> clear() {
+		init();
+		return this;
+	}
+
+
+	public list<T> insert(iterator it, T t) throws RuntimeException {
+		Node node, prevNode, nextNode;
+		nextNode = (Node)it;
+		prevNode = nextNode.prev;
+		if (prevNode == null)
+			throw new RuntimeException(iteratorError);
+		node = new Node(t);
+		prevNode.next = node;
+		node.prev = prevNode;
+		node.next = nextNode;
+		nextNode.prev = node;
+		++size;
+		return this;
+	}
+
+
+	public list<T> erase(iterator it) throws RuntimeException {
+		Node node, prevNode, nextNode;
+		node = (Node)it;
+		prevNode = node.prev;
+		nextNode = node.next;
+		if (prevNode == null || nextNode == null)
+			throw new RuntimeException(iteratorError);
+		prevNode.next = nextNode;
+		nextNode.prev = prevNode;
+		--size;
+		return this;
+	}
+
+
+	public list<T> push_back(T t) throws RuntimeException {
+		return insert(end(), t);
+	}
+
+
+	public list<T> pop_back() throws RuntimeException {
+		return erase(rbegin());
+	}
+
+
+	public list<T> push_front(T t) throws RuntimeException {
+		return insert(begin(), t);
+	}
+
+
+	public list<T> pop_front() throws RuntimeException {
+		return erase(begin());
+	}
+
+
+	public list<T> reverse() {
+		Node node, nodeTmp;
+		nodeTmp = head;
+		head = tail;
+		tail = nodeTmp;
+
+		node = head;
+		while (node != tail) {
+			nodeTmp = node.prev;
+			node.prev = node.next;
+			node.next = nodeTmp;
+			node = node.next;
+		}
+
+		nodeTmp = node.prev;
+		node.prev = node.next;
+		node.next = nodeTmp;
+		return this;
+	}
+
+
+	public list<T> sort(Comparator<? super T> c) {
+		if (size <= 1)
+			return this;
+		ArrayList<Node> na = new ArrayList<Node>();
+		na.ensureCapacity(size);
+		Node node = head.next;
+		for (int i=0; i<size; ++i) {
+			na.add(node);
+			node = node.next;
+		}
+		na.sort((n1, n2)->{
+				return c.compare(n1.data, n2.data);
+			}
+		);
+
+		node = na.get(0);
+		head.next = node;
+		node.prev = head;
+		node.next = na.get(1);
+
+		node = na.get(size - 1);
+		tail.prev = node;
+		node.prev = na.get(size - 2);
+		node.next = tail;
+
+		for (int i=1; i<size-1; ++i) {
+			na.get(i).prev = na.get(i-1);
+			na.get(i).next = na.get(i+1);
+		}
+		return this;
+	}
+
+
+	private static final String iteratorError = "[ 24k list ] >>> iterator out of bound";
+
+}
---
+++ STL/list/2.0/1.0/xx1.java
@@ -0,0 +1,41 @@
+public class xx1{
+	public static void main(String[] args) {
+		list<String> L = new list<String>();
+
+		for (int i=0; i<100; ++i)
+			L.push_back(""+i);
+
+		L.insert(L.rbegin(), "24k");
+		L.pop_back();
+
+		// 排序
+		L.sort((s1, s2)->{ return s1.compareTo(s2); });
+
+		// 迭代器
+		list<String>.iterator it;
+
+		// 遍历输出
+		it = L.begin();
+		while (it != L.end()) {
+			print(it.data + " ");
+			it = it.next();
+		}
+		println();
+		println();
+
+		L.reverse(); // 倒置
+
+		// 遍历输出
+		it = L.begin();
+		while (it != L.end()) {
+			print(it.data + " ");
+			it = it.next();
+		}
+		println();
+	}
+
+	static <T> void print(T t) { System.out.print(t); }
+	static <T> void println() { System.out.println(); }
+	static <T> void println(T t) { System.out.println(t); }
+	static void printf(String fmt, Object... args) { System.out.printf(fmt, args); }
+}
